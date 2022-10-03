9100b51ea8141d656159627d2d334e03a65a666b
timf34 <farrelti@tcd.ie>
2022-10-02 23:01:40 -0400

    plotting something... will come back to this tmrw

--- okayama_dataset.py
+++ okayama_dataset.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 from typing import Union, List
 
@@ -33,13 +33,53 @@ class OkayamaDataset:
         # This gets the indices of the values not equal to 0.0
         return x.ne(0.0)[x.ne(0.0)].index  # Source: https://stackoverflow.com/questions/52173161/getting-a-list-of-indices-where-pandas-boolean-series-is-true
 
+    def get_values_between_two_rows(self, start_index: int, finish_index: int) -> pd.DataFrame:
+        return self.csv_data.iloc[start_index:finish_index]
+
+    def get_lists_for_plot(self):
+        df = self.get_values_between_two_rows(2, 6548)
+
+        # Note: this is really bad code but I just want to get something working for now... I don't know why I found this so hard today
+        brake_list = self.convert_series_to_list(df['Brake'])
+        throttle_list = self.convert_series_to_list(df['Throttle'])
+        speed_list = self.convert_series_to_list(df['Speed'])
+
+        # What even should the x-axis be? I'm just going to use the index for now
+        x = [i for i in range(len(brake_list))]
+
+        return brake_list, throttle_list, speed_list, x
+
+    def plot_lists(self, brake_list, throttle_list, speed_list, x) -> None:
+        """
+        Plots the lists
+        :param brake_list:
+        :param throttle_list:
+        :param speed_list:
+        :param x:
+        :return:
+        """
+        fig, ax = plt.subplots()
+        ax.plot(x, brake_list, label='Brake')
+        ax.plot(x, throttle_list, label='Throttle')
+        ax.plot(x, speed_list, label='Speed')
+        ax.legend()
+        plt.show()
+
+
+
+
+    @staticmethod
+    def convert_series_to_list(series: pd.Series) -> List:
+        return series.tolist()
 
 
 def main():
     dataset = OkayamaDataset(cleaned_file=True)
     # print(dataset.get_headers())
     # print(dataset.get_data())
-    dataset.get_rows_where_value_changes('Lap No.')
+    # print(dataset.get_rows_where_value_changes('Lap No.'))
+    # print(dataset.get_values_between_two_rows(2, 6548))
+    dataset.plot_lists(*dataset.get_lists_for_plot())
 
 
 if __name__ == '__main__':
