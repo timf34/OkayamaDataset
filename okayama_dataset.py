47d620e0c8180583a0be91ed37977c2ab2122507
timf34 <farrelti@tcd.ie>
2022-10-02 22:01:25 -0400

    func for getting indices where the lap no. changes

--- okayama_dataset.py
+++ okayama_dataset.py
@@ -1,6 +1,7 @@
-import csv
 import pandas as pd
 import matplotlib as mpl
+
+from typing import Union, List
 
 
 class OkayamaDataset:
@@ -12,22 +13,33 @@ class OkayamaDataset:
         self.csv_data = self.read_csv()
 
     def read_csv(self):
-        with open(self.filepath, 'r') as f:
-            print("type of csv reader: ", type(csv.reader(f)))
-            return list(csv.reader(f))
+        return pd.read_csv(self.filepath, header=9)
 
     def get_headers(self):
-        return self.csv_data[9]
+        return self.csv_data.columns
 
-    def get_data(self):
-        return self.csv_data[10:]
+    def get_rows_where_value_changes(self, column: Union[str, int]) -> List[int]:
+        """
+        Returns a list of the indices where the value of the column changes
+        :param column: The column to check for changes
+        :return: The row where the value changes
+        """
+        if isinstance(column, str):
+            column = self.get_headers().get_loc(column)
+
+        # This does 3 operations: 1. selects the column, 2. removes NaN values, 3. Calcs the difference between the current and previous row values (with our dataset, this will equal 1.0 when the lap goes up by 1)
+        x = self.csv_data.iloc[:, column].dropna().diff()
+
+        # This gets the indices of the values not equal to 0.0
+        return x.ne(0.0)[x.ne(0.0)].index  # Source: https://stackoverflow.com/questions/52173161/getting-a-list-of-indices-where-pandas-boolean-series-is-true
 
 
 
 def main():
     dataset = OkayamaDataset(cleaned_file=True)
-    print(dataset.get_headers())
+    # print(dataset.get_headers())
     # print(dataset.get_data())
+    dataset.get_rows_where_value_changes('Lap No.')
 
 
 if __name__ == '__main__':
